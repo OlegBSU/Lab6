dcfdd6b0607c37387b54101398d817906c58e85d
OlegBSU <olegstrekachev@u.boisestate.edu>
2026-02-24 10:35:24 -0700

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,9 +1,13 @@
 module adder(
+   input A, B,
     // Declare your A/B inputs
+    output Y, C1
     // Declare Y output
     // Declare carry output
 );
 
+   assign Y = A ^ B;
+   assign C1 = A & B;
     // Enter logic equation here
 
 endmodule
