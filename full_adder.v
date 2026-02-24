1ba958dd7d9eec5c05b99010ce41a90c797529df
OlegBSU <olegstrekachev@u.boisestate.edu>
2026-02-24 10:35:42 -0700

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,1 +1,11 @@
 // Implement module called full_adder
+module full_adder (
+    input A, B, Cin,
+    output Y, Cout
+);
+
+assign Y = Cin ^ A ^ B;
+
+assign Cout = (A & Cin) | (A & B) | (B & Cin);
+
+endmodule
